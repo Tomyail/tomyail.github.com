edfca1e365e351548967dc9b8115598214771dd4
xuexin <tomyail1989@gmail.com>
2018-03-18 19:52:02 +0800

    refactor: 暂时去掉 bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 // Import typefaces
-import 'typeface-montserrat'
-import 'typeface-merriweather'
+import 'typeface-montserrat';
+import 'typeface-merriweather';
 
-import profilePic from './profile-pic.jpg'
-import { rhythm } from '../utils/typography'
+import profilePic from './profile-pic.jpg';
+import { rhythm } from '../utils/typography';
 
 class Bio extends React.Component {
   render() {
+    return <div />;
     return (
       <div
         style={{
           display: 'flex',
-          marginBottom: rhythm(2.5),
+          marginBottom: rhythm(2.5)
         }}
       >
         <img
@@ -23,7 +24,7 @@ class Bio extends React.Component {
             marginRight: rhythm(1 / 2),
             marginBottom: 0,
             width: rhythm(2),
-            height: rhythm(2),
+            height: rhythm(2)
           }}
         />
         <p>
@@ -34,8 +35,8 @@ class Bio extends React.Component {
           </a>
         </p>
       </div>
-    )
+    );
   }
 }
 
-export default Bio
+export default Bio;
