110028d3bdbf4f4e6dd4dbcf46d143e04997eee1
Leo sun <tomyail989@gmail.com>
2021-03-06 15:36:43 +0800

    feat: scroll to top

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,64 +1,90 @@
-import React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import { graphql, useStaticQuery } from 'gatsby';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
-import Slide from '@material-ui/core/Slide';
-import Link from 'gatsby-link';
+import AppBar from "@material-ui/core/AppBar";
+import Slide from "@material-ui/core/Slide";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Zoom from "@material-ui/core/Zoom";
+import { graphql, useStaticQuery } from "gatsby";
+import Link from "gatsby-link";
+import React from "react";
+import Fab from "@material-ui/core/Fab";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 
 interface Props {
-    /**
-     * Injected by the documentation to work in an iframe.
-     * You won't need it on your project.
-     */
-    window?: () => Window;
-    children: React.ReactElement;
+  children: React.ReactElement;
 }
 
 function HideOnScroll(props: Props) {
-    const { children, window } = props;
-    // Note that you normally won't need to set the window ref as useScrollTrigger
-    // will default to window.
-    // This is only being set here because the demo is in an iframe.
-    const trigger = useScrollTrigger({ target: window ? window() : undefined });
+  const { children } = props;
 
-    return (
-        <Slide appear={false} direction="down" in={!trigger}>
-            {children}
-        </Slide>
-    );
+  const trigger = useScrollTrigger();
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+const useStyles2 = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      zIndex: theme.zIndex.appBar,
+      position: "fixed",
+      bottom: theme.spacing(2),
+      right: theme.spacing(2),
+    },
+  })
+);
+
+function ScrollTop(props: Props) {
+  const { children } = props;
+  const classes = useStyles2();
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    const anchor = document.querySelector("#back-to-top-anchor");
+
+    if (anchor) {
+      anchor.scrollIntoView({ behavior: "smooth",block:'center' });
+    }
+  };
+
+  return (
+    <Zoom in={trigger}>
+      <div onClick={handleClick} role="presentation" className={classes.root}>
+        {children}
+      </div>
+    </Zoom>
+  );
 }
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            flexGrow: 1,
-        },
-        menuButton: {
-            marginRight: theme.spacing(2),
-        },
-        title: {
-            flexGrow: 1,
-            '& a': {
-                color:theme.palette.primary.light,
-                textDecoration: 'inherit'
-              },
-              '& a:hover': {
-                textDecoration: 'inherit'
-              }
-        },
-    }),
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+      "& a": {
+        color: theme.palette.primary.light,
+        textDecoration: "inherit",
+      },
+      "& a:hover": {
+        textDecoration: "inherit",
+      },
+    },
+  })
 );
 const Header = () => {
-    const classes = useStyles();
-    const data = useStaticQuery(graphql`
+  const classes = useStyles();
+  const data = useStaticQuery(graphql`
     query HeaderQuery {
       site {
         siteMetadata {
@@ -67,27 +93,26 @@ const Header = () => {
         }
       }
     }
-  `)
-    return (
-        <React.Fragment>
-            {/* <div className={classes.root}> */}
-            <HideOnScroll >
-                <AppBar position="sticky">
-                    <Toolbar>
-                        {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton> */}
-                        <Typography variant="h6" className={classes.title}>
-                            <Link to={'/'}>
-                                {data.site.siteMetadata.title}
-                            </Link>
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
-            </HideOnScroll>
-            {/* </div> */}
-        </React.Fragment>
-    );
-}
+  `);
+  return (
+    <React.Fragment>
+      <HideOnScroll>
+        <AppBar>
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              <Link to={"/"}>{data.site.siteMetadata.title}</Link>
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+      <Toolbar id="back-to-top-anchor" />
+      <ScrollTop>
+        <Fab color="secondary" size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
+    </React.Fragment>
+  );
+};
 
-export default Header
+export default Header;
