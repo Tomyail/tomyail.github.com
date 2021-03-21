b3152b28235ba002a29ec122e8c7397529e068ea
Leo sun <tomyail989@gmail.com>
2021-03-21 23:59:25 +0800

    format date

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -28,7 +28,7 @@ const PostPreview = ({ node }) => {
             <div>{title}</div>
           </Link>
         }
-        subheader={`${node.frontmatter.created_at} | ${node.timeToRead} min read`}
+        subheader={`${new Date(node.frontmatter.created_at).toLocaleDateString()} | ${node.timeToRead} min read`}
       />
       <Box>
         <Typography paragraph variant="body1">
